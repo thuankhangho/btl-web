b65424c9c0c3e69c7c493d3dd68e520e20a89d03
VDTHP <dangcapnhat664@gmail.com>
2022-06-11 22:42:59 +0700

    it's pain
    setting up something for front end

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- jQuery CDN-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <!-- Bootstrap CDN-->
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <title>Bài tập lớn Lập trình Web (CO3049)</title>
+  <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <!-- container -->
+    
+    <div class="container"> 
+        <!-- Navbar -->
+        <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+            Ramen Something
+        </a>
+        </nav>
+
+
+    </div> <!-- end .container -->
+ 
+
+</body>
+</html>
