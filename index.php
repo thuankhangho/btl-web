fa58742b520c41bcd2f6bff8ea4de52f114d2b68
VDTHP <dangcapnhat664@gmail.com>
2022-06-11 21:37:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <!-- jQuery CDN-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <!-- Bootstrap CDN-->
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+    .container{ margin: 0 auto;}
+    </style>
+</head>
     <?php
     session_start();
     include('header.php');
