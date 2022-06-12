b27113b0aa03f5b18dde7a241f574e2e744df9cb
VDTHP <dangcapnhat664@gmail.com>
2022-06-12 11:08:09 +0700

    nothing new
    still nav bar

--- index.php
+++ index.php
@@ -6,47 +6,57 @@
     <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    
     <!-- Bootstrap CDN-->
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <title>Bài tập lớn Lập trình Web (CO3049)</title>
+  <!-- CSS -->
   <link rel="stylesheet" href="styles/styles.css">
+  <title>Bài tập lớn Lập trình Web (CO3049)</title>
+  
 </head>
 <body>
-    <!-- container -->
     
-    <div class="container"> 
         <!-- Navbar -->
-
-
-    <nav class="navbar navbar-expand-sm bg-light navbar-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">
-            <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse justify-content-center"  id="collapsibleNavbar">
-    
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Foods</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Something</a>
-        </li>    
-      </ul>
-    </div>
-    </div>
+    <nav class="my-navbar navbar navbar-expand-xl navbar-dark bg-dark">
+    <div class="my-container container-fluid">
+          <a class="navbar-brand" href="index.php">
+              <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-center"  id="collapsibleNavbar">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Menu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link color-nav" href="#">Something</a>
+              </li>    
+            </ul>
+          </div>
+      </div>
     </nav>
-        
-    </div> <!-- end .container -->
+     <!-- end .navbar -->
+
+    <!-- main content --> 
+    <div class="main-content">
+
+    </div>
+    <!-- end .main-content -->    
+
+
+    <!-- footer --> 
+    <div class="footer">
+
+    </div>
+    <!-- end .footer -->   
  
 
 </body>
