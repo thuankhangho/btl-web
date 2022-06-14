f7c9ee149825e9342467cad9e086e6ea4969bb60
VDTHP <dangcapnhat664@gmail.com>
2022-06-14 22:28:41 +0700

    product

--- productInfo.php
+++ productInfo.php
@@ -29,7 +29,7 @@
   <!-- CSS -->
   <link rel="stylesheet" href="styles/product-style.css">
   <script src = "js/script.js"></script>
-  <title>Sản phẩm</title>
+  <title>Thông tin món ăn</title>
 </head>
 
 <body>
@@ -49,7 +49,7 @@
     <div class="container dark-grey-text mt-5">
 
       <!--Grid row-->
-      <div class="row">
+      <div class="row productDISP">
 
         <!--Grid column-->
         <div class="col-md-6 mb-4">
@@ -65,17 +65,28 @@
           <div class="p-4">
 
             <div class="mb-3">
-                <h3><?php echo htmlspecialchars($dish1['name']); ?></h3>
+                <h1><?php echo htmlspecialchars($dish1['name']); ?></h1>
             </div>
 
             <p class="lead">
-              <span><?php echo htmlspecialchars($dish1['price']); ?>.000 VND</span>
+            <span><h4>Giá:</h4><?php echo htmlspecialchars($dish1['price']); ?>.000 VND</span>
             </p>
 
-            <p class="lead font-weight-bold">Description</p>
+            <p class="lead font-weight-bold"><h5>Miêu tả:</h5></p>
 
             <p><?php echo htmlspecialchars($dish1['desciption']); ?></p>
-
+            <p><h5>Tình trạng:</h5>
+                <?php
+                    if($dish1['status'] == 1)
+                    {
+                        echo '<p class="stat-ok">Còn hàng<p>';
+                    }
+                    else
+                    {
+                        echo '<p class="stat-fail">Hết hàng<p>';
+                    }
+                ?>
+            </p>
             <form class="d-flex justify-content-left">
               <!-- Default input -->
               <!-- <input type="number" value="1" aria-label="Search" class="form-control" style="width: 100px"> -->
@@ -97,12 +108,12 @@
       <hr>
 
       <!--Grid row-->
-      <div class="row d-flex justify-content-center wow fadeIn">
+      <div class="row d-flex justify-content-center commentDISP">
 
         <!--Grid column-->
         <div class="col-md-6 text-center">
 
-          <h4 class="my-4 h4">Additional information</h4>
+          <h4 class="my-4 h4">Comment</h4>
 
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus suscipit modi sapiente illo soluta odit
             voluptates,
@@ -119,25 +130,16 @@
 
         <!--Grid column-->
         <div class="col-lg-4 col-md-12 mb-4">
-
-          <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/11.jpg" class="img-fluid" alt="">
-
         </div>
         <!--Grid column-->
 
         <!--Grid column-->
         <div class="col-lg-4 col-md-6 mb-4">
-
-          <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/12.jpg" class="img-fluid" alt="">
-
         </div>
         <!--Grid column-->
 
         <!--Grid column-->
         <div class="col-lg-4 col-md-6 mb-4">
-
-          <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/13.jpg" class="img-fluid" alt="">
-
         </div>
         <!--Grid column-->
 
