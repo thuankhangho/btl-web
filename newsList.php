7573b731987846656806c12818853d0db9c1c6f1
TitaniumBull224 <titaniumyama224@gmail.com>
2022-06-14 23:48:33 +0700

    Update newsList.php

--- newsList.php
+++ newsList.php
@@ -1,9 +1,9 @@
 <?php
-    include 'config/dtb.php';
+    require_once('config/config.php');
     //select data
-    $query1 = "SELECT id, name, price FROM product ORDER BY id";
-    $res = mysqli_query($conn,$query1);
-    $food = mysqli_fetch_all($res,MYSQLI_ASSOC);
+    $query = "SELECT id, name, price FROM product ORDER BY id";
+    $res = mysqli_query($conn, $query);
+    $food = mysqli_fetch_all($res, MYSQLI_ASSOC);
     $row_cnt = $res->num_rows;
     mysqli_free_result($res);
 ?> 
@@ -27,39 +27,45 @@
 </head>
 
 <body>
-<!-- nav bar --> 
-<div>
-      <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/btl-web/";
-      include($IPATH."navbar.php");?>
-    </div>
-    <!-- end nav bar -->
-    <div class="displayProd">
-        <div class="row">
-          <?php
-          if($row_cnt==0){
-                echo "<div class='alert alert-danger'>No records found.</div>";
+  <!-- nav bar --> 
+  <div>
+    <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/btl-web/";
+    include($IPATH."navbar.php");?>
+  </div>
+  <!-- end nav bar -->
+  <div class="displayProd">
+      <div class="row">
+        <?php
+          if($row_cnt == 0) {
+            echo "<div class='alert alert-danger'>No records found.</div>";
           }
-            ?>
-          <?php foreach($food as $dish){?>
-            <div class="col s-6 md-3">
-                <div class="card z-depth-0">
-                  <div class="card-content center">
-                    <h6><?php echo htmlspecialchars($dish['name']); ?></h6>
-                    <div><?php echo htmlspecialchars($dish['price']); ?>
-                  </div>
-                  <div class="card-action right-align">
-                        <a class="brand-text" href="#">More info</a>
-                  </div>
-                </div>
+          foreach($food as $dish) { 
+        ?>
+
+        <div class="col s-6 md-3">
+            <div class="card z-depth-0">
+              <div class="card-content center">
+                <h6><?php echo htmlspecialchars($dish['name']); ?></h6>
+                <div><?php echo htmlspecialchars($dish['price']); ?></div>
+              </div>
+              <div class="card-action right-align">
+                    <a class="brand-text" href="#">More info</a>
+              </div>
             </div>
-          <?php } ?>
         </div>
-    <!-- footer --> 
-    <div>
-          <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/btl-web/";
-          include($IPATH."footer.php");?>
-        </div>
-    <!-- end footer --> 
-    </div>
+
+        <?php 
+          }
+          mysqli_close($conn); 
+        ?>
+      </div>
+  </div>
+  <!-- footer --> 
+  <div>
+    <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/btl-web/";
+    include($IPATH."footer.php");?>
+  </div>
+  <!-- end footer --> 
+  
 </body>
 </html>
