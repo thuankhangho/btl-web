89725f7ccd18a6187a226f4e06db39de10e60171
thuankhangho <hothuankhang@gmail.com>
2022-06-14 13:47:11 +0700

    Updated index

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <!-- CSS -->
   <link rel="stylesheet" href="styles/styles.css">
-  <script src = "js/script.js"></script>
   <title>Trang chủ</title>
 </head>
 
