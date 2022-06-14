db56b570efc492db037cba1f7f305093c4ca8efb
VDTHP <dangcapnhat664@gmail.com>
2022-06-14 10:21:47 +0700

    major change

--- index.php
+++ index.php
@@ -17,52 +17,15 @@
 </head>
 
 <body>
-    <!-- Navbar -->
-    <nav class="my-navbar-logo navbar navbar-expand-xl navbar-dark bg-dark">
-          <div class="my-container container-fluid justify-content-center">
-          <a class="navbar-brand" href="index.php">
-              <img src="img/logo.png" width="90" height="90" class="d-inline-block align-top" alt="">
-          </a>
-          </div>
-    </nav>
-    <nav class="my-navbar navbar navbar-expand-xl navbar-dark bg-dark">
-    <div class="my-container container-fluid justify-content-center">
-          
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-              <span class="navbar-toggler-icon justify-content-center"></span>
-          </button>
-      <div class="collapse navbar-collapse justify-content-center"  id="collapsibleNavbar">
-          <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Trang chủ</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="php/about.php">Giới thiệu</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="php/product.php">Sản phẩm</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="news/news.php">Tin tức</a>
-              </li>
-               <!-- Icon dropdown -->
-               <li class="dropdown user-drop">
-                  <button type="button" class="user-btn btn btn-dark dropdown-toggle" data-bs-toggle="dropdown">
-                  <i class="fas fa-user-circle"></i>
-                  </button>
-                    <ul class="dropdown-menu dropdown-menu-dark">
-                      <li><a class="dropdown-item" href="php/form/register.php">Đăng ký</a></li>
-                      <li><a class="dropdown-item" href="php/form/login.php">Đăng nhập</a></li>
-                    </ul>
-               </li>
-           </ul>
-       </div>
+    <!-- nav bar --> 
+    <div>
+      <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/btl-web/";
+      include($IPATH."navbar.html");?>
     </div>
-    </nav>
-    <!-- end .navbar -->
-
+    <!-- end nav bar --> 
     <!-- main content --> 
     <div class="main-content">
+
             <div class=" food-search row height d-flex justify-content-center align-items-center">
 
                 <div class="col-md-8">
@@ -113,43 +76,11 @@ Sed interdum hendrerit ante sed gravida. Quisque est augue, mattis ac mi nec, el
          </div>
     </div>
     <!-- end .main-content -->    
-
     <!-- footer --> 
-    <div class="footer text-center">
-        
-        <div class="container p-4">
-    <!-- Section: Social media -->
-            <div class="mb-4">
-                <!-- Facebook -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                  ><i class="fab fa-facebook-f"></i
-                ></a>
-
-                <!-- Twitter -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                  ><i class="fab fa-twitter"></i
-                ></a>
-
-                <!-- Google -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                  ><i class="fab fa-google"></i
-                ></a>
-
-                <!-- Instagram -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                  ><i class="fab fa-instagram"></i
-                ></a>
-
-                <!-- Linkedin -->
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                  ><i class="fab fa-linkedin-in"></i
-                ></a>
-            </div>
-        <p>orem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Donec Hendrerit Ligula Nunc, Non Consequat Lorem Aliquam A. Vivamus Non Consequat Urna. Fusce Lobortis Ultrices Tristique. Nullam Ante Leo, Pulvinar Et Eleifend A, Congue Quis Libero. Mauris Nec Ullamcorper Justo. Etiam Sit Amet Finibus Nulla, In Ullamcorper Sem. Proin Quis Mauris Ac Neque Tempus Ultricies. Etiam Suscipit Tincidunt Metus Elementum Volutpat. Sed Interdum Hendrerit Ante Sed Gravida. Quisque Est Augue, Mattis Ac Mi Nec, Eleifend Venenatis Lorem. Morbi Lobortis Placerat Commodo. In Luctus Mi Purus. Donec Non Eros In Velit Faucibus Vulputate. Nam Eu Est Vulputate, Congue Justo Ut, Sodales Orci. Praesent Eu Fermentum Massa. Phasellus Ac Volutpat Lacus. Sed Porttitor</p>
-        <!-- Section: Social media -->
-        <p style="font-weight: bold; color: white">©2022 All Rights Reserved</p>
-        </div>
+    <div>
+      <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/btl-web/";
+      include($IPATH."footer.html");?>
     </div>
-    <!-- end .footer -->
+    <!-- end footer --> 
 </body>
 </html>
