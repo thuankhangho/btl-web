71360824452e2f56b3b0eb82fe8d93d7801bb2d8
TitaniumBull224 <titaniumyama224@gmail.com>
2022-06-15 13:24:21 +0700

    add header to contain all bootstrap whatever

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- include library -->
+  <?php include ('header.php') ?>
   <!-- CSS -->
   <link rel="stylesheet" href="styles/footer.css">
   <title>Document</title>
--- register.php
+++ register.php
@@ -1,17 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <!-- Font Awesome CDN-->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" rel="stylesheet">
-  <!-- jQuery CDN-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <!-- jsdelivr CDN / Sweet Alert2-->
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <!-- Bootstrap CDN-->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- include library -->
+  <?php include ('header.php') ?>
   <!-- CSS -->
   <link rel="stylesheet" href="styles/form.css">
   <script src = "js/.js"></script>
