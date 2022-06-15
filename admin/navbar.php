dbab23681620b4b844b70755896bcf9190ff28ac
Matt Ciriazo <daokhang2002@gmail.com>
2022-06-15 12:33:38 +0700

    Stuff

--- admin/navbar.php
+++ admin/navbar.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- CSS -->
-  <link rel="stylesheet" href="../styles/styles.css">
+  <link rel="stylesheet" href="admin-styles/adminButton.css">
     <title>ADMIN DASHBOARD</title>
 </head>
 <body>
