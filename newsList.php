759d9d495647eeec084776dc1b22df27f0a3db21
VDTHP <dangcapnhat664@gmail.com>
2022-06-16 16:15:12 +0700

    Update newsList.php

--- newsList.php
+++ newsList.php
@@ -47,7 +47,7 @@
       <div class="col-md-6 mb-4">
         <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
           <img src="" class="img-fluid" />
-          <a href="newsInfo.php?news_id=<?php echo htmlspecialchars($newspage1['id']); ?>">
+          <a href="">
             <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
           </a>
         </div>
