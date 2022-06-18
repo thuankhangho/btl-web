d8a8917d236f28f5accea3cdf3906816cdf6cf73
Matt Ciriazo <daokhang2002@gmail.com>
2022-06-18 12:52:05 +0700

    Update newsInfo.php

--- newsInfo.php
+++ newsInfo.php
@@ -79,9 +79,9 @@
     }
   ?>
   <div id="container">
-    <div class="newsDISP" style="padding: 0 0 0 1em">
+    <div class="newsDISP" style="padding: 1em 0 0 2em; background-color: #eb4d4b">
         <div class="mb-3 text-white">
-            <h1><?php echo htmlspecialchars($newspage1['name']); ?></h1>
+            <h1 style="font-size: 50px"><?php echo htmlspecialchars($newspage1['name']); ?></h1>
         </div>
         <p class="lead text-white">
           <span class="text-white">
@@ -89,8 +89,33 @@
           </span>
         </p>
         </div>
-        <p><?php echo htmlspecialchars($newspage1['content']); ?></p>
-        <hr>
+        <div class="row">
+          <div class="col-sm-7" style="border: 1px solid black; padding: 1em 2em 0 2em; margin: 1em 2em 0 2em; font-size: 20px">
+          <p><?php echo htmlspecialchars($newspage1['content']); ?>
+          <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat mi sed luctus tempus. Duis elementum dictum lacus, condimentum imperdiet urna tristique a. Fusce sollicitudin lacinia ligula, ac tincidunt sem interdum ac. Morbi molestie nisl lectus. Nam ex erat, facilisis sit amet ultrices a, bibendum vel tellus. Pellentesque sit amet sapien risus. In fringilla augue egestas tellus dapibus, et ullamcorper augue mattis. Nulla ac purus in velit blandit tempus ut sit amet nibh. Aenean malesuada, leo non lacinia vehicula, libero arcu tincidunt leo, sed semper diam enim quis dolor.
+          <br>Curabitur quis tortor suscipit, efficitur lectus eu, ultricies eros. Duis pharetra id purus a aliquet. Suspendisse potenti. Pellentesque convallis nibh vitae auctor ornare. Suspendisse luctus lectus interdum neque cursus, at rutrum augue egestas. Sed sodales ligula eget enim dapibus vestibulum. Ut auctor sit amet eros vel bibendum. Duis pretium placerat tortor nec cursus. Morbi ac arcu vitae urna scelerisque mollis hendrerit scelerisque dolor. Morbi hendrerit felis eu nulla auctor viverra.
+          <br>Nullam mattis ex quis libero vehicula, ac dignissim ipsum eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce commodo porta tempor. Suspendisse eu tortor augue. Suspendisse potenti. Suspendisse sed efficitur augue. Phasellus dignissim tincidunt leo, id pellentesque orci lacinia ut. Morbi dui lacus, iaculis quis est sit amet, tincidunt ullamcorper nunc. Integer magna elit, porta eget euismod in, egestas vel augue. Sed vel libero congue, porta lorem ut, porta sem. Duis in velit tristique, pellentesque mauris fermentum, molestie nisl. Proin tempor lacinia sem.</p>
+          </div>
+          <div class="col-sm-4" style="padding: 0 20px 0 10px; margin-top: 10px">
+    <div class="row">
+      <br><br>
+    </div>
+      <div class="row" style="font-weight: bold; font-size: 30px; display: flex; justify-content: center; align-items: center; font-family: 'Charis SIL', serif;">
+        HỖ TRỢ TRỰC TUYẾN
+      </div>
+      <div class="row" style="font-size: 20px; background-color: #2ed573; border: 1px solid black; display: flex">
+        <p style="padding-top: 10px"><i class="fa fa-phone"> Mr. Sơn: (+84)822 870 179</i><br>
+        <i class="fa fa-phone"> Mr. Khang: (+84)838 374 577</i>
+      </p>
+      </div>
+      <div class="row" style="font-size: 20px; background-color: #ffa502; border: 1px solid black; display: flex">
+        <p style="padding-top: 10px"><i class="fas fa-envelope"> Mr. Khang: khang.lamborghini@hcmut.edu.vn</i><br>
+        <i class="fas fa-envelope"> Mr. Toàn: toan.vu1805@hcmut.edu.vn</i>
+      </p>
+      </div>
+    </div>
+        </div>
+
         <div class="container my-5 py-5">
           <div class="row d-flex justify-content-center commentDISP">
 
